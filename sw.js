073a6876d3eb1a5a8be6bf9eeb3630488a9d72be
Maxkaiju123 <Maxkaiju123@users.noreply.github.com>
2026-02-11 14:56:06 +0800

    Initial commit for PWA

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'water-calc-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
